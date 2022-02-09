a35a611e6c7fde74af59867c4469b83650ccfc19
Nando Vieira <me@fnando.com>
2022-02-08 22:03:18 -0800

    Retry failed requests.

--- src/helpers/youtube.ts
+++ src/helpers/youtube.ts
@@ -10,15 +10,41 @@ export type Channel = {
   subscriberCount: number;
 };
 
+async function retry(fn: () => Promise<Response>): Promise<unknown> {
+  return new Promise((resolve, reject) => {
+    let attempts = 0;
+
+    const run = async () => {
+      attempts += 1;
+
+      try {
+        const result = await fn();
+        resolve(result);
+      } catch (error) {
+        if (attempts < 3) {
+          setTimeout(run, 3000 * attempts);
+          return;
+        }
+
+        reject(error);
+      }
+    };
+
+    run();
+  });
+}
+
 export async function getBroadcasts(
   settings: GlobalSettings,
 ): Promise<Broadcast[]> {
   const endpoint = settings.apiEndpoint.replace(/\/$/, "");
   const url = `${endpoint}/broadcasts?status=active&_key=camelcase`;
 
-  const response = await fetch(url, {
-    headers: { Authorization: `Bearer ${settings.apiKey}` },
-  });
+  const response = await retry(() =>
+    fetch(url, {
+      headers: { Authorization: `Bearer ${settings.apiKey}` },
+    }),
+  );
   const broadcasts = await response.json();
 
   return broadcasts;
@@ -28,9 +54,11 @@ export async function getChannel(settings: GlobalSettings): Promise<Channel> {
   const endpoint = settings.apiEndpoint.replace(/\/$/, "");
   const url = `${endpoint}/channel?_key=camelcase`;
 
-  const response = await fetch(url, {
-    headers: { Authorization: `Bearer ${settings.apiKey}` },
-  });
+  const response = await retry(() =>
+    fetch(url, {
+      headers: { Authorization: `Bearer ${settings.apiKey}` },
+    }),
+  );
   const channel = await response.json();
 
   return channel;
